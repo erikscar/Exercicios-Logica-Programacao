8d2200f3ac914351b69174f316fb2087d0fc58ce
erikscar <erikscarcela@gmail.com>
2025-03-31 10:52:37 -0300

    Exercicio: Contando Ovelhas

---
+++ Contando-Ovelhas/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercicios 
+{
+    //Consider an array/list of sheep where some sheep may be missing from their place.
+    // We need a function that counts the number of sheep present in the array (true means present).
+    public class ContandoOvelhas 
+    {
+        public static void Main(string[] args)
+        {
+            var sheeps = new bool[] { true, false, true };
+            Console.WriteLine(CountSheeps(sheeps));
+        }
+
+        public static int CountSheeps(bool[] sheeps)
+        {
+            int aux = 0;
+
+            for(int i = 0; i < sheeps.Length; i++)
+            {
+                if(sheeps[i] == true) aux++;
+            }
+
+            return aux;
+        }
+    }
+}
