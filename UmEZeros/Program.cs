c22eaa224e09adeeeb5dea0be92cf34254b023be
erikscar <erikscarcela@gmail.com>
2025-04-01 12:58:43 -0300

    Exercicio: Um e Zeros

---
+++ UmEZeros/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace UmEZeros
+{
+    class Program {
+        public static void Main(string[] args)
+        {
+            int[] Test = new int[] {0,1,0,1};
+            Console.WriteLine(binaryArrayToNumber(Test));
+        }
+
+        public static int binaryArrayToNumber(int[] array)
+        {
+            int convertNumber = 0;
+            for(int i = 0; i < array.Length; i++)
+            {
+                int pow = (int)Math.Pow(2, array.Length - i - 1);
+
+                convertNumber += array[i] *  pow;
+            }
+            return convertNumber;
+        }
+    }
+}
