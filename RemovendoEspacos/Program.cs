074debe1c7f392d5e5830cd661db8576ab706295
erikscar <erikscarcela@gmail.com>
2025-04-02 10:27:13 -0300

    Exercicio: Removendo Espaços de Strings

---
+++ RemovendoEspacos/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace RemovendoEspacos
+{
+    //Write a function that removes the spaces from the string, then return the resultant string.
+    class Program 
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(SpacesRemover("8 j 8   mBliB8g  imjB8B8  jl  B"));
+        }
+
+        public static string SpacesRemover(string input)
+        {
+            
+            //return input.Replace(" ", "");
+            string aux = "";
+            for(int i = 0; i < input.Length; i ++)
+            {
+                if(input[i].ToString() != " ")
+                {
+                    aux += input[i];
+                }
+            }
+            return aux;
+        }
+    }
+}
