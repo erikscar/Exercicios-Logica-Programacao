cd5ae93155c28e321b53d88c130c9c26fe1dbe90
erikscar <erikscarcela@gmail.com>
2025-04-10 14:46:07 -0300

    Exercicio: Encontrando a Agulha

---
+++ BrincandoComDigitos/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace BrincandoComDigitos
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(DigPow(46288, 3));
+        }
+
+        public static long DigPow(int n, int p)
+        {
+           var aux = n.ToString();
+           double total = 0;
+
+           for(int i = 0; i < aux.Length; i++)
+           {
+           total += Math.Pow(double.Parse(aux[i].ToString()), p + i);
+           }
+
+           if(total % n == 0)
+           {
+                return (long)total / n;
+           }
+
+           return -1;
+        }
+    }
+}
---
+++ EncontreAgulha/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace EncontreAgulha
+{
+    //Can you find the needle in the haystack?
+    //Write a function findNeedle() that takes an array full of junk but containing one "needle"
+    //After your function finds the needle it should return a message (as a string) that says:
+    //"found the needle at position " plus the index it found the needle
+    
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Object[] haystack = new Object[]{"junk", "more junk", new String("needle"), "gadget"};
+            Console.WriteLine(FindNeedle(haystack));
+        }
+
+        public static string FindNeedle(object[] haystack)
+        {
+            // int index = 0;
+            // for(int i = 0; i < haystack.Length; i++)
+            // {
+            //     if(Equals(haystack[i], "needle"))
+            //     {
+            //         index = i;
+            //     }
+            // }
+            // return $"found the needle at position {index}";
+
+            return $"found the needle at position {Array.IndexOf(haystack, "needle")}";
+        }
+    }
+}
