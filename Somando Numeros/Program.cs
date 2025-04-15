84808ad310a08b642f0392ea6ea05d8c44bfeeb6
erikscar <erikscarcela@gmail.com>
2025-04-15 13:14:08 -0300

    Somando Números de um Array

---
+++ Somando Numeros/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SomandoNumeros {
+    class Program {
+        public static void Main(string[] args)
+        {
+            double[] array = new double[]{1, 5.2, 4, 0, -1};
+            
+            Console.WriteLine(SumArray(array));
+        }
+
+        public static double SumArray(double[] array)
+        {
+            return array == null ? 0 : array.Sum();
+        }
+    }
+}
