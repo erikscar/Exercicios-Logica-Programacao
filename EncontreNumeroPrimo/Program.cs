dfafaebdc86c7db2ee3c9c6190b5e7c10ff6541f
erikscar <erikscarcela@gmail.com>
2025-04-16 13:06:12 -0300

    Exercicio: Encontrando o Nuero Primo

---
+++ EncontreNumeroPrimo/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace EncontreNumeroPrimo;
+
+//Given an array of integers, find the one that appears an odd number of times.
+//There will always be only one integer that appears an odd number of times.
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(FindIt([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1]));
+    }
+
+    public static int FindIt(int[] seq)
+    {
+
+        for (int i = 0; i < seq.Length - 1; i++)
+        {
+            int counter = 0;
+
+            for (int j = 0; j < seq.Length; j++)
+            {
+                if (seq[i] == seq[j])
+                {
+                    counter++;
+                }
+            }
+
+            if (counter % 2 != 0)
+                {
+                    return seq[i];
+                }
+        }
+
+        return -1;
+    }
+}
--- LadosIguaisArray/Program.cs
+++ LadosIguaisArray/Program.cs
@@ -1,5 +1,8 @@
-﻿namespace La
+﻿namespace LadosIguaisArray
 {
+    //You are going to be given an array of integers. Your job is to take that array and find an index N where the sum of the integers to the left of N is equal to the sum of the integers to the right of N.
+    //If there is no index that would make this happen, return -1.
+
     class Program
     {
         public static void Main(string[] args)
